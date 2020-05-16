d1dbe3378685f1fc398722f6342d13dea740be77
USER <sofiyaaaaa7@gmail.com>
2020-05-16 17:34:07 +0600

    Add Js and HTML/CSS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(document).ready(function () {
+        $("a.nav__link").click(function () {
+            $("html,body").animate({
+                scrollTop: $($(this).attr("href")).offset().top + "px"
+            }, {
+                duration: 1000,
+                easing: "swing"
+            });
+            return false;
+        });
+    });
+
+});
